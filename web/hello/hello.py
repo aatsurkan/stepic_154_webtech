ed77f201b22a8aec298238a0599decc3262232aa
aatsurkan <aatsurkan@mail.ru>
2016-05-20 14:46:44 +0300

    2.7 done / small fix 27_django_auth.txt and hello.py

--- web/hello/hello.py
+++ web/hello/hello.py
@@ -1,9 +1,6 @@
 def app(env, start_response):
   status = '200 OK'
-  headers = [
-    ('Content-Type', 'text/plain')
-  ]
-  body = env['QUERY_STRING'].split('&')
-  body = [item + "\r\n" for item in body]
+  headers = [('Content-Type', 'text/plain')]
+  opt = '\r\n'.join(env['QUERY_STRING'].split('&')).encode('utf-8')
   start_response(status, headers)
-  return body 
+  return [opt] 
